87502817c717c578a93dfa1447d6ec0ba92fb857
GGolfz <wisarutgolf11@gmail.com>
2021-05-05 19:50:29 +0700

    add: edit user email, firstname, and lastname :sparkles:

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,17 +1,38 @@
 class Api::UserController < ApplicationController
   include Token
+  include Response
   def index
     token = request.headers['Authorization'].split(' ').last
     user_id = extract_token(token)
     @user = User.find_by(id: user_id)
     @user.password = nil
-    render json: {
+    success_response({
       user: @user,
       token: token
-    }, status: 200
+    })
   end
 
   def update
+    token = request.headers['Authorization'].split(' ').last
+    user_id = extract_token(token)
+    body = JSON.parse(request.body.read)
+    email = body["email"]
+    firstname = body["firstname"]
+    lastname = body["lastname"]
+    @current = User.find_by(email: email)
+    if @current && @current.id != user_id
+      error_response('This email is already taken')
+      return
+    end
+    @user = User.find_by(id: user_id)
+    @user.email = email
+    @user.firstname = firstname
+    @user.lastname = lastname
+    @user.save
+    @user.password = nil
+    success_response({
+      user: @user
+    })
   end
 
   def password
