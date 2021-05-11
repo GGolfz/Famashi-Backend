e1d77e804f18d2397d9857ad32b714ecc8b11dff
GGolfz <wisarutgolf11@gmail.com>
2021-05-11 22:29:16 +0700

    update: register API & testing :sparkles:

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -8,14 +8,12 @@ class Api::AuthController < ApplicationController
     body = JSON.parse(request.body.read)
     email = body["email"]
     password = body["password"]
-    firstname = body["firstname"]
-    lastname = body["lastname"]
     hashedPassword = BCrypt::Password.create(password)
     if User.find_by(email: email)
       error_response('This email is already taken')
       return
     end
-    @user = User.create(email: email,password: hashedPassword, firstname: firstname, lastname: lastname)
+    @user = User.create(email: email,password: hashedPassword)
     UserInfo.create(users_id:@user.id)
     # Todo: Generate User Reminder Here
     token = generate_token(@user.id)
