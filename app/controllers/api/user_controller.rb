b574fc1d7df2126ca4ead2717bac3138411cec57
GGolfz <wisarutgolf11@gmail.com>
2021-05-13 23:15:01 +0700

    add: update user profile :sparkles:

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -19,10 +19,11 @@ class Api::UserController < ApplicationController
   def update
     token = request.headers['Authorization'].split(' ').last
     user_id = extract_token(token)
-    body = JSON.parse(request.body.read)
+    body = params
     email = body["email"]
     firstname = body["firstname"]
     lastname = body["lastname"]
+    upload = body["upload"]
     @current = User.find_by(email: email)
     if @current && @current.id != user_id
       error_response('This email is already taken')
@@ -33,7 +34,18 @@ class Api::UserController < ApplicationController
       error_response('Unauthorize', 401)
       return
     end
-    @user.update(email:email,firstname:firstname,lastname:lastname)
+    filename = @user.profile_pic
+    if upload == 'true'
+      upload_file = body['profile_img']
+      ext = upload_file.original_filename.split('.').last
+      filename = user_id.to_s + "-" + Time.now.to_i.to_s + "." + ext.to_s 
+      File.delete(Rails.root.join('public','uploads','profile_img', @user.profile_pic))
+      File.open(Rails.root.join('public', 'uploads','profile_img', filename.to_s ), 'wb') do |file|
+        file.write(upload_file.read)
+      end
+    end
+
+    @user.update(email:email,firstname:firstname,lastname:lastname,profile_pic:filename)
     @user.password = nil
     success_response({
       user: @user
@@ -44,10 +56,15 @@ class Api::UserController < ApplicationController
     token = request.headers['Authorization'].split(' ').last
     user_id = extract_token(token)
     body = JSON.parse(request.body.read)
-    password = body["password"]
+    old_password = body["old_password"]
+    password = body["new_password"]
     @user = User.find_by(id: user_id)
     if @user == nil
       error_response('Unauthorize', 401)
+      return
+    end
+    if @user.password != old_password 
+      error_response('Wrong Password',400)
       return
     end
     hashedPassword = BCrypt::Password.create(password)
@@ -58,6 +75,4 @@ class Api::UserController < ApplicationController
     })
   end
 
-  def image
-  end
 end
